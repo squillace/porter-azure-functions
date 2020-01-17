a42498ad3902139918d46e7e53498fa1f7302d42
Ralph Squillace <ralph@squillace.com>
2020-01-17 18:40:36 -0500

    working; with signatures off; keep at it

--- pkg/functions/build.go
+++ pkg/functions/build.go
@@ -1,5 +1,7 @@
 package functions
 
+import "fmt"
+
 // This is an example. Replace thie following with whatever steps are needed to
 // install required components into
 // const dockerfileLines = `RUN apt-get update && \
@@ -15,6 +17,17 @@ package functions
 // Build will generate the necessary Dockerfile lines
 // for an invocation image using this mixin
 func (m *Mixin) Build() error {
-	//fmt.Fprintf(m.Out, dockerfileLines)
+
+	fmt.Fprintln(m.Out, `RUN apt-get update`)
+	fmt.Fprintln(m.Out, `RUN apt-get install -y curl gnupg gnupg2 apt-transport-https`)
+	//fmt.Fprintln(m.Out, `RUN apt-get install -y `)
+	fmt.Fprintln(m.Out, `RUN curl https://packages.microsoft.com/keys/microsoft.asc -o foo.asc`)
+	fmt.Fprintln(m.Out, `RUN gpg foo.asc > microsoft.gpg`)
+	fmt.Fprintln(m.Out, `RUN mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg`)
+	fmt.Fprintln(m.Out, `RUN echo "deb [arch=amd64] https://packages.microsoft.com/debian/9/prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list`)
+	//fmt.Fprintln(m.Out, `RUN cat /etc/apt/sources.list.d`)
+	fmt.Fprintln(m.Out, `RUN apt-get update`)
+	fmt.Fprintln(m.Out, `RUN apt-get install -y --allow-unauthenticated azure-functions-core-tools`)
+
 	return nil
 }
